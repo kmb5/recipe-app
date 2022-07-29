370e3491bf66c5ca984ffe16a89327cbd8d5889e
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-07-29 11:58:21 +0200

    main

--- recipe_app/main.py
+++ recipe_app/main.py
@@ -1,12 +1,36 @@
+from pathlib import Path
+import os
+
 import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi_sqlalchemy import db
+
+from .models.models import Recipe as ModelRecipe
+from .schema import Recipe as SchemaRecipe
+
+BASE_DIR = Path(__file__).parent.parent
+load_dotenv(Path(BASE_DIR, ".env"))
 
 app = FastAPI()
 
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
-@app.post("/recipe/", response_model=Recipe)
-def create_recipe(recipe: Recipe):
-    return recipe
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong"}
+
+
+@app.post("/recipe/", response_model=SchemaRecipe)
+def create_recipe(recipe: SchemaRecipe):
+    db_recipe = ModelRecipe(
+        name=recipe.name, cooking_time_min=recipe.cooking_time_min, num_people=recipe.num_people, kcal=recipe.kcal
+    )
+    db.session.add(db_recipe)
+    db.session.commit()
+    return db_recipe
 
 
 if __name__ == "__main__":
