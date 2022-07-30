2ee5702d6242d7b5eb5c488bffc5a91ea0e05b0b
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-07-30 17:11:31 +0200

    add first routes

--- recipe_app/main.py
+++ recipe_app/main.py
@@ -1,42 +1,76 @@
 from pathlib import Path
-import os
+from typing import Any
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request, Depends
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
-from fastapi_sqlalchemy import DBSessionMiddleware
-from fastapi_sqlalchemy import db
 
-from .models.models import Recipe as ModelRecipe
-from .schema import Recipe as SchemaRecipe
+from recipe_app import crud
+from recipe_app import deps
+from recipe_app.schemas.recipe import Recipe, RecipeCreate
 
-BASE_DIR = Path(__file__).parent.parent
-load_dotenv(Path(BASE_DIR, ".env"))
+# Project Directories
+ROOT = Path(__file__).resolve().parent.parent
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-app = FastAPI()
+load_dotenv(Path(ROOT, ".env"))
 
-app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+app = FastAPI(title="Recipe API")
+api_router = APIRouter()
 
 
-@app.get("/ping")
-def ping():
+@api_router.get("/ping", status_code=200)
+def ping() -> dict:
     return {"ping": "pong"}
 
 
-@app.post("/recipe/", response_model=SchemaRecipe)
-def create_recipe(recipe: SchemaRecipe):
-    db_recipe = ModelRecipe(
-        name=recipe.name, cooking_time_min=recipe.cooking_time_min, num_people=recipe.num_people, kcal=recipe.kcal
-    )
-    db.session.add(db_recipe)
-    db.session.commit()
-    return db_recipe
+@api_router.get("/", status_code=200)
+def root(request: Request, db: Session = Depends(deps.get_db)) -> dict:
+    """
+    Root GET
+    """
+    recipes = crud.recipe.get_multi(db=db, limit=10)
+    return TEMPLATES.TemplateResponse("index.html", {"request": request, "recipes": recipes})
 
 
-@app.get("/recipes/", response_model=list[SchemaRecipe])
-def get_recipes():
-    return db.session.query(ModelRecipe).all()
+@api_router.get('/recipe/{recipe_id}', status_code=200, response_model=Recipe)
+def fetch_recipe(*, recipe_id: int, db: Session = Depends(deps.get_db)) -> Any:
+    """
+    Fetch a single recipe by ID
+    """
+    result = crud.recipe.get(db=db, id=recipe_id)
+    if not result:
+        raise HTTPException(
+            status_code=404, detail=f"Recipe with id {recipe_id} not found")
+    return result
 
+
+@api_router.post('/recipe/', status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate, db: Session = Depends(deps.get_db)) -> dict:
+    """
+    Create a new recipe in the database
+    """
+
+    recipe = crud.recipe.create(db=db, obj_in=recipe_in)
+
+    return recipe
+
+    # @app.post("/recipe/", response_model=RecipeBase)
+    # def create_recipe(recipe: Recipe):
+    #     recipe = CRUDRecipe(Recipe).create()
+    #     db_recipe = Recipe(
+    #         name=recipe.name, cooking_time_min=recipe.cooking_time_min, num_people=recipe.num_people, kcal=recipe.kcal
+    #     )
+    #     db.session.add(db_recipe)
+    #     db.session.commit()
+    #     return db_recipe
+    # @app.get("/recipes/", response_model=list[SchemaRecipe])
+    # def get_recipes():
+    #     return db.session.query(ModelRecipe).all()
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
