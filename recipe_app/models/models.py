8b91d6d3ddff659abe8e4377345b5ea307dd5f31
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-08-01 14:09:39 +0200

    add user

--- recipe_app/models/models.py
+++ recipe_app/models/models.py
@@ -2,14 +2,6 @@ from typing import Optional
 from sqlmodel import Field, SQLModel, Relationship
 
 
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    username: str
-    email: str = Field(default=None)
-    is_superuser: bool = Field(default=False)
-    recipes: list["Recipe"] = Relationship(back_populates="submitter")
-
-
 class RecipeBase(SQLModel):
     name: str = Field(index=True)
     cooking_time_min: int
