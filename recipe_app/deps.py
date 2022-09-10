73762bed09153cdccb3b223c4b2fa2aec755e5b7
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-09-10 12:55:21 +0200

    no need deps

--- recipe_app/deps.py
+++
@@ -1,8 +0,0 @@
-from typing import Generator
-from sqlmodel import Session
-from recipe_app.db.session import engine
-
-
-def get_session() -> Generator:
-    with Session(engine) as session:
-        yield session
