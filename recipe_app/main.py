0aad03df6c4e0772fc02e677bdc377262f7219bf
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-09-10 12:53:54 +0200

    move crud to main

--- recipe_app/main.py
+++ recipe_app/main.py
@@ -1,13 +1,12 @@
 from pathlib import Path
-from uuid import UUID
+from uuid import UUID, uuid4
 import uvicorn
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, Query
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 
-from recipe_app.crud.base import RecipeRepository, IngredientRepository
-from recipe_app.schemas.recipe_ingredient import RecipeSchemaIn, RecipeSchemaOut, IngredientSchemaIn, IngredientSchemaOut
-from recipe_app.models.models import Recipe, Ingredient
+from recipe_app.schemas.recipe_ingredient import RecipeSchemaIn, RecipeSchemaOut, IngredientSchemaOut
+from recipe_app.models.models import Recipe
 
 
 # Project Directories
@@ -27,38 +26,56 @@ def setup_tables():
         Recipe.create_table(read_capacity_units=1,
                             write_capacity_units=1, wait=True)
 
-    if not Ingredient.exists():
-        Ingredient.create_table(read_capacity_units=1,
-                                write_capacity_units=1, wait=True)
-
 
 @api_router.get("/ping", status_code=200)
 def ping() -> dict:
     return {"ping": "pong"}
 
 
+@api_router.get('/recipes', status_code=200, response_model=list[RecipeSchemaOut])
+def get_all_recipes(limit: int = Query(default=100, lte=100)) -> list[RecipeSchemaOut]:
+    items = Recipe.scan(limit=limit)
+    schemas = []
+
+    for model in items:
+        model.ingredients = [IngredientSchemaOut(
+            **ingredient.attribute_values) for ingredient in model.ingredients]
+        schemas.append(RecipeSchemaOut(**model.attribute_values))
+
+    return schemas
+
+
 @api_router.get('/recipes/{recipe_id}', status_code=200, response_model=RecipeSchemaOut)
 def get_recipe(recipe_id: UUID) -> RecipeSchemaOut:
-    recipe_out = RecipeRepository.get(recipe_id)
-    return recipe_out
+    model = Recipe.get(str(recipe_id))
+    model.ingredients = [IngredientSchemaOut(
+        **ingredient.attribute_values) for ingredient in model.ingredients]
+
+    return RecipeSchemaOut(**model.attribute_values)
 
 
 @api_router.post('/recipes', status_code=201, response_model=RecipeSchemaOut)
 def create_recipe(recipe_in: RecipeSchemaIn) -> RecipeSchemaOut:
-    recipe_out = RecipeRepository.create(recipe_in)
-    return recipe_out
+    dct = recipe_in.dict()
+    dct["id"] = str(uuid4())
+    model = Recipe(**dct)
+    model.save()
+
+    model.ingredients = [IngredientSchemaOut(
+        **ingredient) for ingredient in model.ingredients]
+
+    return RecipeSchemaOut(**model.attribute_values)
 
 
-@api_router.get('/ingredients/{recipe_id}', status_code=200, response_model=IngredientSchemaOut)
-def get_ingredient(ingredient_id: UUID) -> IngredientSchemaOut:
-    ingredient_out = IngredientRepository.get(ingredient_id)
-    return ingredient_out
+@api_router.get('/ingredients', status_code=200, response_model=list[IngredientSchemaOut])
+def get_all_ingredients() -> list[IngredientSchemaOut]:
+    all_recipes = get_all_recipes(limit=None)
+    all_ingredients = []
 
+    for recipe in all_recipes:
+        all_ingredients.extend(recipe.ingredients)
 
-@api_router.post('/ingredients', status_code=201, response_model=IngredientSchemaOut)
-def create_recipe(ingredient_in: IngredientSchemaIn) -> IngredientSchemaOut:
-    ingredient_out = IngredientRepository.create(ingredient_in)
-    return ingredient_out
+    return all_ingredients
 
 
 @api_router.get("/", status_code=200)
@@ -66,145 +83,8 @@ def root(request: Request) -> dict:
     """
     Root GET
     """
-    recipes = RecipeRepository.get_all()
+    recipes = get_all_recipes(limit=None)
     return TEMPLATES.TemplateResponse("index.html", {"request": request, "recipes": recipes})
-
-
-# @api_router.get('/ingredients/{ingredient_id}', status_code=200, response_model=IngredientReadWithRecipe)
-# def read_ingredient(ingredient_id: int, db_session: Session = Depends(deps.get_session)):
-#     """
-#     Read a single ingredient by ID
-#     """
-#     result = db_session.get(Ingredient, ingredient_id)
-#     if not result:
-#         raise HTTPException(
-#             status_code=404, detail=f"Ingredient with id {ingredient_id} not found")
-#     return result
-
-
-# @api_router.get('/ingredients/', status_code=200, response_model=list[IngredientReadWithRecipe])
-# def get_all_ingredients(offset: int = 0, limit: int = Query(default=100, lte=100), db_session: Session = Depends(deps.get_session)):
-
-#     return db_session.exec(select(Ingredient).offset(offset).limit(limit)).all()
-
-
-# @api_router.get('/recipes/', status_code=200, response_model=list[RecipeReadWithIngredients])
-# def get_all_recipes(offset: int = 0, limit: int = Query(default=100, lte=100), db_session: Session = Depends(deps.get_session)):
-#     return db_session.exec(select(Recipe).offset(offset).limit(limit)).all()
-
-
-# @api_router.get('/recipes/{recipe_id}', status_code=200, response_model=RecipeReadWithIngredients)
-# def read_recipe(recipe_id: int, db_session: Session = Depends(deps.get_session)):
-#     """
-#     Read a single recipe by ID
-#     """
-#     result = db_session.get(Recipe, recipe_id)
-#     if not result:
-#         raise HTTPException(
-#             status_code=404, detail=f"Recipe with id {recipe_id} not found")
-#     return result
-
-
-# @api_router.get('/recipes/{recipe_id}/ingredients', status_code=200, response_model=list[Ingredient])
-# def read_recipe(recipe_id: int, db_session: Session = Depends(deps.get_session)):
-#     """
-#     Read a single ingredient by ID
-#     """
-#     result = db_session.get(Recipe, recipe_id)
-#     if not result:
-#         raise HTTPException(
-#             status_code=404, detail=f"Recipe with id {recipe_id} not found")
-
-#     return result.ingredients
-
-
-# @api_router.post('/recipes/{recipe_id}/ingredients', status_code=200, response_model=RecipeReadWithIngredients)
-# def add_ingredients_for_recipe(recipe_id: int, ingredients: list[IngredientForRecipe], db_session: Session = Depends(deps.get_session)):
-#     recipe = db_session.get(Recipe, recipe_id)
-#     if not recipe:
-#         raise HTTPException(
-#             status_code=404, detail=f"Recipe with id {recipe_id} not found")
-
-#     for ingredient in ingredients:
-#         db_ingredient = Ingredient.from_orm(ingredient)
-#         db_ingredient.recipe_id = recipe.id
-
-#         db_session.add(db_ingredient)
-#         db_session.commit()
-#         db_session.refresh(db_ingredient)
-#     return db_session.get(Recipe, recipe_id)
-
-
-# @api_router.post('/ingredients/', status_code=201, response_model=IngredientReadWithRecipe)
-# def create_ingredient(ingredient_in: IngredientCreate, db_session: Session = Depends(deps.get_session)):
-#     """Create a new ingredient in the database"""
-
-#     db_ingredient = Ingredient.from_orm(ingredient_in)
-#     db_session.add(db_ingredient)
-#     db_session.commit()
-#     db_session.refresh(db_ingredient)
-#     return db_ingredient
-
-
-# @api_router.patch("/ingredients/{ingredient_id}", response_model=IngredientRead)
-# def update_ingredient(ingredient_id: int, ingredient: IngredientUpdate, db_session: Session = Depends(deps.get_session)):
-#     db_ingredient = db_session.get(Ingredient, ingredient_id)
-#     if not db_ingredient:
-#         raise HTTPException(status_code=404, detail="Ingredient not found")
-#     ingredient_data = ingredient.dict(exclude_unset=True)
-#     for k, v in ingredient_data.items():
-#         setattr(db_ingredient, k, v)
-#     db_session.add(db_ingredient)
-#     db_session.commit()
-#     db_session.refresh(db_ingredient)
-#     return db_ingredient
-
-
-# @api_router.delete("/ingredients/{ingredient_id}")
-# def delete_ingredient(ingredient_id: int, db_session: Session = Depends(deps.get_session)):
-#     ingredient = db_session.get(Ingredient, ingredient_id)
-#     if not ingredient:
-#         raise HTTPException(status_code=404, detail="Ingredient not found")
-#     db_session.delete(ingredient)
-#     db_session.commit()
-#     return {"ok": True}
-
-
-# @api_router.post('/recipes/', status_code=201, response_model=RecipeCreateWithIngredients)
-# def create_recipe(recipe_in: RecipeCreateWithIngredients, db_session: Session = Depends(deps.get_session)):
-#     """Create a new ingredient in the database"""
-
-#     db_recipe = Recipe.from_orm(recipe_in)
-#     db_session.add(db_recipe)
-#     db_session.commit()
-#     db_session.refresh(db_recipe)
-#     return db_recipe
-
-# --------
-
-# @api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
-# def search_recipes(
-#     *,
-#     keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
-#     max_results: Optional[int] = 10,
-#     db_session: Session = Depends(deps.get_session)
-# ) -> dict:
-#     recipes = crud.recipe.get_multi(db=db_session, limit=max_results)
-#     if not keyword:
-#         return {"results": recipes}
-
-#     results = filter(lambda recipe: keyword.lower()
-#                      in recipe.name.lower(), recipes)
-#     return {"results": list(results)[:max_results]}
-
-
-# @api_router.post('/recipe/', status_code=201, response_model=RecipeInDBBase)
-# def create_recipe(*, recipe_in: RecipeBase, db_session: Session = Depends(deps.get_session)) -> dict:
-#     """
-#     Create a new recipe in the database
-#     """
-
-#     print(recipe_in)
 
 
 app.include_router(api_router)
