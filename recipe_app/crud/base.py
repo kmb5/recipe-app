2a2fd4d36438bdccba81bf2d838c7b5e57928ed7
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-09-10 11:05:58 +0200

    update base crud

--- recipe_app/crud/base.py
+++ recipe_app/crud/base.py
@@ -1,68 +1,58 @@
-from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
+import time
+import uuid
+from typing import Dict, Any, Union
 
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from sqlmodel import select, Session, SQLModel
-
-from recipe_app.db.base_class import Base
-
-ModelType = TypeVar("ModelType", bound=Base)
-CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
-UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+from recipe_app.schemas.recipe_ingredient import RecipeSchemaIn, RecipeSchemaOut, IngredientSchemaIn, IngredientSchemaOut
+from recipe_app.models.models import Recipe, Ingredient
 
 
-class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: Type[ModelType]):
-        """CRUD object with default methods to create, read, update, delete
+class RecipeRepository:
+    table: Recipe = Recipe
+    schema_out: RecipeSchemaOut = RecipeSchemaOut
 
-        Parameters
-        ----------
-        model : A SQLAlchemy model class
-        schema: A Pydantic model (schema) class
-        """
+    @staticmethod
+    def _preprocess_create(values: Dict[str, Any]) -> Dict[str, Any]:
+        timestamp_now = time.time()
+        values["id"] = str(uuid.uuid4())
+        values["created_at"] = timestamp_now
+        values["updated_at"] = timestamp_now
 
-        self.model = model
+        return values
 
-    def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        return db.exec(select(self.model))
-        #db.query(self.model).filter(self.model.id == id).first()
+    @classmethod
+    def create(cls, recipe_in: RecipeSchemaIn) -> RecipeSchemaOut:
+        data = cls._preprocess_create(recipe_in.dict())
+        model: Recipe = cls.table(**data)
+        model.save()
+        return cls.schema_out(**model.attribute_values)
 
-    def get_multi(self, db: Session, skip: int = 0, limit: int = 100) -> List[ModelType]:
-        return db.exec(select(self.model).offset(skip).limit(limit)).all()
+    @classmethod
+    def get(cls, entry_id: Union[str, uuid.UUID]) -> RecipeSchemaOut:
+        model = cls.table.get(str(entry_id))
+        return cls.schema_out(**model.attribute_values)
 
-    def create(self, db: Session, *, obj_in: SQLModel) -> ModelType:
-        db_obj = SQLModel.from_orm(obj_in)
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
 
-    def get_or_create(self, db: Session, obj_in: CreateSchemaType) -> ModelType:
-        instance = self.get(db=db, name=obj_in.name)
-        if instance:
-            return instance
-        else:
-            self.create(db=db, obj_in=obj_in)
+class IngredientRepository:
+    table: Ingredient = Ingredient
+    schema_out: IngredientSchemaOut = IngredientSchemaOut
 
-    def update(self, db: Session, *, db_obj: ModelType, obj_in: Union[UpdateSchemaType, Dict[str, Any]]) -> ModelType:
-        obj_data = jsonable_encoder(db_obj)
-        if isinstance(obj_in, dict):
-            update_data = obj_in
-        else:
-            update_data = obj_in.dict(exclude_unset=True)
+    @staticmethod
+    def _preprocess_create(values: Dict[str, Any]) -> Dict[str, Any]:
+        timestamp_now = time.time()
+        values["id"] = str(uuid.uuid4())
+        values["created_at"] = timestamp_now
+        values["updated_at"] = timestamp_now
 
-        for field in obj_data:
-            if field in update_data:
-                setattr(db_obj, field, update_data[field])
+        return values
 
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
+    @classmethod
+    def create(cls, ingredient_in: IngredientSchemaIn) -> IngredientSchemaOut:
+        data = cls._preprocess_create(ingredient_in.dict())
+        model: Recipe = cls.table(**data)
+        model.save()
+        return cls.schema_out(**model.attribute_values)
 
-        return db_obj
-
-    def remove(self, db: Session, *, id: int) -> ModelType:
-        obj = db.query(self.model).get(id)
-        db.delete(obj)
-        db.commit()
-        return obj
+    @classmethod
+    def get(cls, entry_id: Union[str, uuid.UUID]) -> IngredientSchemaOut:
+        model = cls.table.get(str(entry_id))
+        return cls.schema_out(**model.attribute_values)
