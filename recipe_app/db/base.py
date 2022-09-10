f3038b5988443fae645889a8449a8541f5970615
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-09-10 12:09:06 +0200

    db unneeded

--- recipe_app/db/base.py
+++
@@ -1,6 +0,0 @@
-
-# Import all the models, so that Base has them before being
-# imported by Alembic
-from recipe_app.db.base_class import Base  # noqa
-from recipe_app.models.user import User  # noqa
-from recipe_app.models.recipe import Recipe  # noqa
