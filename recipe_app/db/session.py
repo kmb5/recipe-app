4f00a7352d2160b01103d270de35b72c49c21d7a
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2022-09-10 10:42:45 +0200

    change session to pynamodb

--- recipe_app/db/session.py
+++ recipe_app/db/session.py
@@ -1,12 +1,21 @@
 import os
 from pathlib import Path
-from sqlmodel import create_engine
-from sqlalchemy.orm import sessionmaker
+import boto3
 from dotenv import load_dotenv
+from boto3.resources.base import ServiceResource
 
 BASE_DIR = Path(__file__).parent.parent.parent
 load_dotenv(Path(BASE_DIR, ".env"), verbose=True)
 
 DB_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(DB_URL)
+
+def initialize_db() -> ServiceResource:
+    db = boto3.resource(
+        'dynamodb',
+        region_name=os.getenv('DB_REGION_NAME'),
+        aws_access_key_id=os.getenv('DB_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('DB_SECRET_ACCESS_KEY')
+    )
+
+    return db
